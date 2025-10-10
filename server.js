ddf98f8a2a345c8127f83b0ff6f378a2e10ccb07
zainnasseer <zainnaser18@gmail.com>
2025-10-10 21:33:25 +0300

    rateLimit.windowsMS

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
   ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-  message: `too many requests please try again in ${windowMs / 1000} minutes.`,
+  message: `too many requests please try again in ${rateLimit.windowMs / 1000} minutes.`,
 });
 
 // Apply the rate limiting middleware to all requests.
